d4090391c910b3f3e1ba7f6c9e5d4e4918befb80
Jinyeol Kim <jinyul80@gmail.com>
2025-04-07 23:39:00 +0900

    간단한 챗봇 기능 구현
    - Linkedin 데이터 조회 후 요약

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,6 +1,32 @@
 import os
+from langchain.prompts import PromptTemplate
+from langchain_groq import ChatGroq
+from langchain_core.output_parsers.string import StrOutputParser
+
+from third_parties.linkedin import scrape_linkedin_profile
+
 
 if __name__ == "__main__":
     print("hello Langchain!")
 
-    print("API_KEY: ", os.environ["GROQ_API_KEY"])
+    summary_template = """
+        given the Linkedin information {information} about a person from I want you to create:
+        1. a short summary
+        2. two interesting facts about them
+        
+        모든 대답은 한글로 대답해 주세요.
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatGroq(temperature=0, model="qwen-2.5-coder-32b")
+
+    chain = summary_prompt_template | llm | StrOutputParser()
+
+    linkedin_data = scrape_linkedin_profile("", mock=True)
+
+    res = chain.invoke(input={"information": linkedin_data})
+
+    print(res)
